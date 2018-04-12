e39b93bf6384903964bfa3e456706495da8fa5b9
LeAnnS <leann.sawatzky@gmail.com>
2018-04-12 02:36:38 -0500

    revised images in friends.json file and updated title

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ class App extends Component {
   render() {
     return (
       <Wrapper>
-        <Title>Friends List</Title>
+        <Title>MAD MEN MEMORY GAME</Title>
         {this.state.friends.map(friend => (
           <FriendCard
             removeFriend={this.removeFriend}
